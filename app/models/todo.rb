730221942b0df46178c0c46338c99cced0070f96
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2014-09-24 12:22:52 -0500

    Make todos default to 7 days left

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,20 +1,19 @@
 class Todo < ActiveRecord::Base
   validates :description, length: {minimum: 5 }, presence: true
   belongs_to :user
+  after_initialize :init
 
   default_scope { order('complete ASC') }
   default_scope { order('days_left DESC') }
   #This I guess lets me call these as methods later? Although I'm not sure where I'd call them...sticking to default for now
   #scope :ordered_by_complete, -> { order ('complete ASC') }
   #scope :ordered_by_days_left, -> { order ('days_left DESC') }
+  def init
+    self.days_left  ||= 7
+  end
 
   def toggle_complete
-    if self.complete?
-      self.complete = false
-    else
-      self.complete = true
-    end
-
+    self.complete? ? self.complete = false : self.complete = true
     self.save
   end
 end
